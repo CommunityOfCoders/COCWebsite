e1b432863e8065bdf5df115b8625b3b3a2233849
Pratik <prabhupratik27@gmail.com>
2019-09-26 10:46:03 +0530

    chnages to home1

--- client/src/components/HomeComponents/Home1.vue
+++ client/src/components/HomeComponents/Home1.vue
@@ -12,8 +12,8 @@
 
           <img src="../../assets/coclogowithname.png" alt="Vuetify.js" height="auto" class="hidden-sm-and-down" width="80%">
           <img src="../../assets/coc_logo.png" alt="Vuetify.js" height="200" class="hidden-md-and-up">
-          <h1 class="white--text mb-2 display-1 text-xs-center">Community Of Coders</h1>
-          <div class="subheading mb-3 text-xs-center">Good software, like wine, takes time ~ Joel Spolsky</div>
+          <h1 class="white--text mb-2 display-1 text-center">Community Of Coders</h1>
+          <div class="subheading mb-3 text-center">Good software, like wine, takes time ~ Joel Spolsky</div>
           <v-btn
             class="blue lighten-2 mt-5"
             dark
