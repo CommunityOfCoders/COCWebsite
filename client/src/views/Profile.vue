132d88b6e7f13239295767763be99164ac9b8d53
Pratik <prabhupratik27@gmail.com>
2019-09-28 22:14:20 +0530

    Tried a bit of profile

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <p>Yes this is profile page</p>
+    <div v-html="identicon" class="profilepic"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted () {
+    this.textInput = this.$store.state.user
+  },
+  data: () => ({
+    textInput: ''
+  }),
+  computed: { // Turn data into viewable values
+    identicon: function() {
+      const html = jdenticon.toSvg(this.textInput, 200);
+      console.log(html)
+      return html
+    }
+  }
+}
 </script>
 
 <style scoped>
+svg {
+  display: block;
+  border: 1px solid block;
+  width: 1000px;
+  height: 1000px;
+}
 </style>
