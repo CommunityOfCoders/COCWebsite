5eca236efa57ef8d19dc31bfa98d22afbabf8f6d
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-10-17 07:18:29 +0530

    Added Blogs server logic

---
+++ server/src/controllers/Blogs.js
@@ -0,0 +1,56 @@
+const User = require('../models/User')
+const Blog = require('../models/Blog')
+
+module.exports = {
+    async allBlogs (_req,res) {
+        const blogs = await Blog.find();
+		res.json(blogs);
+    },
+    async viewBlogById (req,res) {
+        const blogId = req.params.id;
+		const blog = await Blog.findById(blogId);
+		res.json(blog);
+    },
+    // async newBlog (req,res) {
+        
+    // },
+    async uploadBlog (req,res) {
+        // TODO: add isBlogAuthorized middleware
+        try {
+            // Assumed that req.body already has required fields
+            req.body.author = req.user.username;
+            const blog = await Blog.create(req.body);
+			res.json({
+				"id": blog._id
+			});
+		} catch (err) {
+			res.status(400).send({
+				err: err
+			})
+		}
+    },
+    // async editBlog (req,res) {
+        
+    // },
+    async editBlogById (req,res) {
+        // TODO: add isBlogAuthorized middleware
+        try {
+            // Assumed that req.body already has required fields
+            const blog = await Blog.findByIdAndUpdate(req.params.id, req.body);
+            res.json({
+				"id": blog._id
+			});
+		} catch (err) {
+			res.status(400).send({
+				err: err
+			})
+		}
+    },
+    async deleteBlogById (req,res) {
+        // TODO: add isBlogAuthorized middleware
+        const blogId = req.params.id;
+		const blog = await Blog.findById(blogId);
+		await blog.remove();
+		res.status(204);
+    },
+}
