7df04f8cee1f90777f0ce1b3163ee745f71f4f6b
Pratik Prabhu <prabhupratik27@gmail.com>
2020-02-03 20:26:49 +0530

    Added a git directory

---
+++ client/dist/precache-manifest.0b86690efd8c72082b55b23570c1ad27.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f736edc020068094021cd05413307d11",
+    "url": "/img/section.f736edc0.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "24909e5f74d91328d1d0c13c66069699",
+    "url": "/img/coclogowithname.24909e5f.png"
+  },
+  {
+    "revision": "858fa742774e5eb35bde",
+    "url": "/css/chunk-vendors.728b1051.css"
+  },
+  {
+    "revision": "30dd0f529e5155cab8a1aefa4716de7f",
+    "url": "/img/tui-editor.30dd0f52.png"
+  },
+  {
+    "revision": "b4361244b610df3a6c728a26a49f782b",
+    "url": "/img/tui-editor-2x.b4361244.png"
+  },
+  {
+    "revision": "895a1f8c5ed329ddb5f9",
+    "url": "/js/app.9c1ba89a.js"
+  },
+  {
+    "revision": "858fa742774e5eb35bde",
+    "url": "/js/chunk-vendors.f4f77453.js"
+  },
+  {
+    "revision": "ffec432016d09b9ac44d9bd71e620611",
+    "url": "/img/hero.ffec4320.jpeg"
+  },
+  {
+    "revision": "7143a49f2107ad0eb01aaca79a9fff12",
+    "url": "/img/eventBack.7143a49f.jpg"
+  },
+  {
+    "revision": "417ff2aabd8cd374daee0fcc836c9864",
+    "url": "/img/coc_logo.417ff2aa.png"
+  },
+  {
+    "revision": "66f88190ac16c715d7202a2d73cd6e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895a1f8c5ed329ddb5f9",
+    "url": "/css/app.28c4e00b.css"
+  }
+];
