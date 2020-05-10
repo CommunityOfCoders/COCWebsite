fe868e2db7fcd8bb652a5b9a5d27490af3100e9f
pal_16 <mantrypalak@gmail.com>
2020-05-10 20:04:39 +0530

    added blogs and glimpses* section(get request done) , refactored and attatched links to all component

--- new_client/src/components/Header.js
+++ new_client/src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 import './Header.css';
 export default function Header() {
   return (
     <div className="navbar">
-        <div className="navbar-right">
-          <ul>
-            <a className="active" href="\#"><i className="fa fa-fw fa-home"></i> HOME</a> 
-            <a href="/#"><i className="fa fa-fw fa-info"></i> ABOUT US</a>
-            <a href="/#"><i className="fa fa-fw fa-terminal"></i> ACTIVITIES</a> 
-            <a href="/#"><i className="fa fa-fw fa-picture-o"></i> GLIMPSES</a> 
-            <a href="/#"><i className="fa fa-fw fa-users"></i> TEAM</a>
-            <a href="/#"><i className="fa fa-fw fa-sign-in"></i> SIGN UP</a>
-          </ul>
-        </div>
+      <div className="navbar-right">
+        <Link to="/"><i className="fa fa-fw fa-info"></i> HOME</Link>
+        <Link to="/about"><i className="fa fa-fw fa-info"></i> ABOUT US</Link>
+
+        <Link to="/"><i className="fa fa-fw fa-info"></i>EVENTS</Link>
+        <Link to="/glimps"><i className="fa fa-fw fa-info"></i>GLIMPSES</Link>
+        <Link to="/blogs"><i className="fa fa-fw fa-info"></i>BLOGS</Link>
+        <Link to="/auth"><i className="fa fa-fw fa-info"></i> SIGN UP</Link>
+
+      </div>
     </div>
   );
 }
