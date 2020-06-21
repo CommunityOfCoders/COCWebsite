3705d4ad0d685147590b79fb8c8f778124a89af8
AtharvaThorve <aaathorve@gmail.com>
2020-06-21 17:09:57 +0530

    Image fetching section added

--- new_client/src/components/events/Event.jsx
+++ new_client/src/components/events/Event.jsx
@@ -4,64 +4,88 @@ import AddEvent from './AddEvent';
 import EventList from './EventList';
 
 class Event extends Component {
-	state = {
-		events: [],
-		isUpdating: false,
-		updatingEvent: null,
-	};
+  state = {
+    events: [],
+    isUpdating: false,
+    updatingEvent: null
+  };
 
-	handleEdit = (eventId) => {
-		let updatingEvent = this.state.events.find(
-			(event) => event._id === eventId
-		);
-		this.setState((prevState) => {
-			updatingEvent = prevState.isUpdating ? null : updatingEvent;
-			return {
-				isUpdating: !prevState.isUpdating,
-				updatingEvent: updatingEvent,
-			};
-		});
-	};
+  handleEdit = (eventId) => {
+    let updatingEvent = this.state.events.find(
+      (event) => event._id === eventId
+    );
+    this.setState((prevState) => {
+      updatingEvent = prevState.isUpdating ? null : updatingEvent;
+      return {
+        isUpdating: !prevState.isUpdating,
+        updatingEvent: updatingEvent
+      };
+    });
+  };
 
-	handleDelete = (eventId) => {
-		axios.delete(process.env.REACT_APP_API + `/events/${eventId}`);
-		const deletedEvent = this.state.events.find(
-			(event) => event._id === eventId
-		);
-		if (
-			JSON.stringify(deletedEvent) ===
-			JSON.stringify(this.state.updatingEvent)
-		) {
-			this.setState({ isUpdating: false, updatingEvent: null });
-		}
-		this.setState({ events: [] });
-	};
+  handleDelete = (eventId) => {
+    axios.delete(process.env.REACT_APP_API + `/events/${eventId}`);
+    const deletedEventIndex = this.state.events.findIndex(
+      (event) => event._id === eventId
+    );
+    if (
+      deletedEventIndex >= 0 &&
+      JSON.stringify(this.state.events[deletedEventIndex]) ===
+        JSON.stringify(this.state.updatingEvent)
+    ) {
+      this.setState({ isUpdating: false, updatingEvent: null });
+    }
+    const events = [...this.state.events];
+    events.splice(deletedEventIndex, 1);
+    this.setState({ events: events });
+  };
 
-	componentDidMount() {
-		axios
-			.get(process.env.REACT_APP_API + '/events')
-			.then((res) => {
-				this.setState({ events: res.data });
-			})
-			.catch((error) => console.log(error));
-	}
+  componentDidMount() {
+    axios
+      .get(process.env.REACT_APP_API + '/events')
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ events: res.data });
+        axios
+          .get('http://res.cloudinary.com/coc-vjti/image/list/event.json')
+          .then((res) => {
+            console.log(res.data.resources);
+            if (this.state.events.length !== 0) {
+              const events = [...this.state.events];
+              for (const event of events) {
+                for (const imageData of res.data.resources) {
+                  if (event._id === imageData.public_id) {
+                    event.version = imageData.version;
+                    break;
+                  }
+                }
+              }
+              this.setState({ events: events });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((error) => console.log(error));
+  }
 
-	render() {
-		return (
-			<div>
-				<EventList
-					events={this.state.events}
-					isUpdating={this.state.isUpdating}
-					handleEdit={this.handleEdit}
-					handleDelete={this.handleDelete}
-				/>
-				<AddEvent
-					isUpdating={this.state.isUpdating}
-					updatingEvent={this.state.updatingEvent}
-				/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <EventList
+          events={this.state.events}
+          isUpdating={this.state.isUpdating}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
+        <AddEvent
+          isUpdating={this.state.isUpdating}
+          updatingEvent={this.state.updatingEvent}
+        />
+      </div>
+    );
+  }
 }
 
 export default Event;
--- server/src/controllers/Events.js
+++ server/src/controllers/Events.js
@@ -2,93 +2,102 @@ const express = require('express');
 const path = require('path');
 const cloudinary = require('cloudinary');
 cloudinary.config({
-	cloud_name: 'coc-vjti',
-	api_key: '552242973352355',
-	api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: 'coc-vjti',
+  api_key: '552242973352355',
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 const Event = require('../models/Event');
 
 module.exports = {
-	async getEvents(_req, res) {
-		const events = await Event.find();
-		res.json(events);
-	},
-	async getEventById(req, res) {
-		try {
-			const eventId = req.params.id;
-			const event = await Event.findById(eventId);
-			res.json(event);
-		} catch (err) {
-			res.status(203).send({
-				err: err,
-			});
-		}
-	},
-	async uploadEvent(req, res) {
-		try {
-			const file = req.file;
-			console.log(req.file);
-			if (file) {
-				const image = cloudinary.v2.uploader.upload(file.path);
-				req.body.image = {
-					url: image.secure_url,
-					public_id: image.public_id,
-				};
-			}
-			const event = await Event.create(req.body);
-			res.json({
-				id: event._id,
-			});
-		} catch (err) {
-			res.status(203).send({
-				err: err,
-			});
-		}
-	},
-	async updateEvent(req, res) {
-		try {
-			const file = req.file;
-			if (file) {
-				const image = cloudinary.v2.uploader.upload(file.path);
-				req.body.image = {
-					url: image.secure_url,
-					public_id: image.public_id,
-				};
-			}
-			const eventId = req.params.id;
-			const event = await Event.findByIdAndUpdate(eventId, req.body);
-			res.json({
-				id: event._id,
-			});
-		} catch (err) {
-			res.status(400).send({
-				err: err,
-			});
-		}
-		// TODO ?
-	},
-	async deleteEvent(req, res) {
-		const eventId = req.params.id;
-		const event = await Event.findById(eventId);
-		await event.remove();
-		res.status(204);
-	},
-	async addForm(req, res) {
-		const formURL = req.body.formURL;
-		const eventId = req.body.id;
+  async getEvents(_req, res) {
+    const events = await Event.find();
+    res.json(events);
+  },
+  async getEventById(req, res) {
+    try {
+      const eventId = req.params.id;
+      const event = await Event.findById(eventId);
+      res.json(event);
+    } catch (err) {
+      res.status(203).send({
+        err: err
+      });
+    }
+  },
+  async uploadEvent(req, res) {
+    try {
+      const file = req.file;
+      console.log(req.file);
+      const event = await Event.create(req.body);
+      if (file) {
+        const image = await cloudinary.v2.uploader.upload(file.path, {
+          public_id: event._id,
+          tags: ['event'],
+          invalidate: true
+        });
+        req.body.image = {
+          url: image.secure_url,
+          public_id: image.public_id
+        };
+      }
+      res.json({
+        id: event._id
+      });
+    } catch (err) {
+      res.status(203).send({
+        err: err
+      });
+    }
+  },
+  async updateEvent(req, res) {
+    try {
+      const eventId = req.params.id;
+      const file = req.file;
+      if (file) {
+        await cloudinary.v2.uploader.destroy(eventId);
+        const image = await cloudinary.v2.uploader.upload(file.path, {
+          public_id: eventId,
+          tags: ['event'],
+          invalidate: true
+        });
+        req.body.image = {
+          url: image.secure_url,
+          public_id: image.public_id
+        };
+      }
+      const event = await Event.findByIdAndUpdate(eventId, req.body);
+      res.json({
+        id: event._id
+      });
+    } catch (err) {
+      res.status(400).send({
+        err: err
+      });
+    }
+  },
+  async deleteEvent(req, res) {
+    const eventId = req.params.id;
+    const event = await Event.findById(eventId);
+    await event.remove();
+    await cloudinary.v2.uploader.destroy(eventId);
+    res.status(204);
+  },
+  async addForm(req, res) {
+    const formURL = req.body.formURL;
+    const eventId = req.body.id;
 
-		try {
-			const event = await Event.findByIdAndUpdate(eventId, {
-				form: formURL,
-			});
+    try {
+      const event = await Event.findByIdAndUpdate(eventId, {
+        form: formURL
+      });
 
-			res.status(200).send({
-				message: 'Form added successfully',
-			});
-		} catch (err) {
-			res.status(203).send({
-				err: err,
-			});
-		}
-	},
+      res.status(200).send({
+        message: 'Form added successfully'
+      });
+    } catch (err) {
+      res.status(203).send({
+        err: err
+      });
+    }
+  }
 };
