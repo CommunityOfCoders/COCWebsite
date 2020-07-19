08e37499740ae5c2238fc4f2899eb548635afb96
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-07-19 20:57:49 +0530

    blogs wip

--- new_client/src/components/blogs/AddBlog.jsx
+++ new_client/src/components/blogs/AddBlog.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import { TextField, Button, Grid } from "@material-ui/core";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import Editor from "./Editor";
+import axios from "axios";
 
 export default function AddBlog() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [blogTitle, setBlogTitle] = useState("");
+  const [blogAuthor, setBlogAuthor] = useState("");
+  const [blogContent, setBlogContent] = useState("**Hello world!!!**");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleDataSubmit = async () => {
+    const blog = {
+      blogTitle: blogTitle,
+      blogContent: blogContent,
+      date: selectedDate,
+      author: blogAuthor,
+    };
+    console.log(blog);
+    const res = await axios.post(
+      process.env.REACT_APP_API + "/blogs/new",
+      JSON.stringify(blog),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(res);
+  };
+
+  return (
+    <Container maxWidth="md" style={{ backgroundColor: "white" }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="date-picker-dialog"
+              label="Date picker dialog"
+              format="MM/dd/yyyy"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            autoFocus
+            label="Enter your username"
+            value={blogAuthor}
+            onChange={(e) => setBlogAuthor(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Enter a title"
+            value={blogTitle}
+            onChange={(e) => setBlogTitle(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Editor content={blogContent} setContent={setBlogContent} />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleDataSubmit}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
--- new_client/src/components/blogs/Blog.jsx
+++ new_client/src/components/blogs/Blog.jsx
@@ -1,40 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import AddBlog from "./AddBlog";
+import { Button, Modal } from "@material-ui/core";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Blog = () => {
-
-	const [posts, setPosts] = useState([]);
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+  const [isModal, toggleIsModal] = useState(false);
   useEffect(() => {
-    axios.get(process.env.REACT_APP_API + "/blogs")
-      .then(res => setPosts(res.data.blogs))
-      .catch(error => console.log(error));
+    axios
+      .get(process.env.REACT_APP_API + "/blogs")
+      .then((res) => setPosts(res.data))
+      .catch((error) => console.log(error));
   }, posts);
 
-	return (
-		<MainContainer>
-
-			{posts.map((article, key) => (
-				<div className="container">
-					<h2>{article.blogTitle}</h2>
-					<p>{article.blogContent}</p>
-					<span className="badge badge-secondary p-2"> {article.author}</span>
-					<div className="row my-4">
-						<div className="col-sm-2">
-							<Link to="/" className="btn btn-outline-success">Edit Blog</Link>
-						</div>
-						<div className="col-sm-2">
-							<Link to="/" className="btn btn-outline-danger">Delete Blog</Link>
-						</div>
-					</div>
-				</div>
-			))}
-		</MainContainer>
-
-	);
+  return (
+    <MainContainer>
+      {posts.map((article, key) => (
+        <div className="container">
+          <h2>{article.blogTitle}</h2>
+          <p>{article.blogContent}</p>
+          <span className="badge badge-secondary p-2"> {article.author}</span>
+          <div className="row my-4">
+            <div className="col-sm-2">
+              <Link to="/" className="btn btn-outline-success">
+                Edit Blog
+              </Link>
+            </div>
+            <div className="col-sm-2">
+              <Link to="/" className="btn btn-outline-danger">
+                Delete Blog
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => toggleIsModal(true)}
+      >
+        Add a new blog
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={isModal}
+        onClose={() => toggleIsModal(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={isModal}>
+          <div className={classes.paper}>
+            <AddBlog />
+          </div>
+        </Fade>
+      </Modal>
+    </MainContainer>
+  );
 };
 export default Blog;
 
 const MainContainer = styled.div`
-margin:7rem 0;`
+  margin: 7rem 0;
+`;
