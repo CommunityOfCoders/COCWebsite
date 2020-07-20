8f24e6c486cf4a5e2247a4498c184e9bfc0cf313
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-07-20 20:11:36 +0530

    blogs wip

--- new_client/src/components/blogs/Blog.jsx
+++ new_client/src/components/blogs/Blog.jsx
@@ -1,89 +1,79 @@
+import "date-fns";
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import AddBlog from "./AddBlog";
-import { Button, Modal } from "@material-ui/core";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+import { Button, Container, Grid, Typography, Card } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
+import ReactMarkdown from "react-markdown";
+import parse from "date-fns/parse";
 
 const Blog = () => {
-  const classes = useStyles();
   const [posts, setPosts] = useState([]);
-  const [isModal, toggleIsModal] = useState(false);
+  const [loading, setIsLoading] = useState(true);
+  const substringSize = 20;
+
   useEffect(() => {
     axios
       .get(process.env.REACT_APP_API + "/blogs")
-      .then((res) => setPosts(res.data))
-      .catch((error) => console.log(error));
-  }, posts);
+      .then((res) => setPosts(res.data.sort((a, b) => b.date - a.date)))
+      .catch((error) => console.log(error))
+      .then(() => setIsLoading(false));
+  }, []);
 
   return (
-    <MainContainer>
-      {posts.map((article, key) => (
-        <div className="container">
-          <h2>{article.blogTitle}</h2>
-          <p>{article.blogContent}</p>
-          <span className="badge badge-secondary p-2"> {article.author}</span>
-          <div className="row my-4">
-            <div className="col-sm-2">
-              <Link to="/" className="btn btn-outline-success">
-                Edit Blog
-              </Link>
-            </div>
-            <div className="col-sm-2">
-              <Link to="/" className="btn btn-outline-danger">
-                Delete Blog
-              </Link>
-            </div>
-          </div>
-        </div>
-      ))}
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => toggleIsModal(true)}
-      >
-        Add a new blog
-      </Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={isModal}
-        onClose={() => toggleIsModal(false)}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={isModal}>
-          <div className={classes.paper}>
-            <AddBlog />
-          </div>
-        </Fade>
-      </Modal>
-    </MainContainer>
+    <Container maxWidth="md">
+      {loading ? (
+        <Skeleton variant="rect" />
+      ) : (
+        <>
+          <Grid container spacing={3}>
+            {posts.map((article, key) => (
+              <Grid item xs={6}>
+                <Card>
+                  <Typography variant="h3">{article.blogTitle}</Typography>
+                  <ReactMarkdown
+                    source={
+                      article.blogContent.length > substringSize
+                        ? `${article.blogContent.substring(
+                            0,
+                            substringSize
+                          )}...`
+                        : article.blogContent
+                    }
+                  />
+                  <span className="badge badge-secondary p-2">
+                    {" "}
+                    {/* {parse(article.date, "dd-MM-YYYY HH:mm:ss", new Date())} */}
+                  </span>
+                  <Typography>Written by : {article.author}</Typography>
+                  <Grid container spacing={4}>
+                    <Grid item xs={6}>
+                      <Button>
+                        <Link to="/" className="btn-outline-success">
+                          Edit Blog
+                        </Link>
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Button>
+                        <Link to="/" className="btn-outline-danger">
+                          Delete Blog
+                        </Link>
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <Button variant="contained" color="primary">
+            <Link to="/addblog" style={{ color: "white" }}>
+              Add a new blog
+            </Link>
+          </Button>
+        </>
+      )}
+    </Container>
   );
 };
 export default Blog;
-
-const MainContainer = styled.div`
-  margin: 7rem 0;
-`;
