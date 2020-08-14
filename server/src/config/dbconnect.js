6a73a5c990f3d4eb6f708c999585a5acaa0616ce
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-08-14 11:44:47 +0530

    Change config for test

--- server/src/config/dbconnect.js
+++ server/src/config/dbconnect.js
@@ -8,9 +8,17 @@ function connect() {
     useUnifiedTopology: true,
     useFindAndModify: false,
   };
-  mongoose.connect(process.env.MONGO_URI, mongooseOptions);
+
+  let connectionString = "mongodb://localhost:27017/test";
+
+  if (process.env.NODE_ENV !== "test") {
+    connectionString = process.env.MONGO_URI;
+  }
+  mongoose.connect(connectionString, mongooseOptions);
   mongoose.Promise = global.Promise;
-  mongoose.connection.on("open", () => console.log("MongoDB Connected"));
+  mongoose.connection.on("open", () =>
+    console.log(`MongoDB Connected`)
+  );
 }
 
 module.exports = connect;
