5f01076982622c83fa9bf471a9eaf5628bd3ff36
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-08-14 13:38:05 +0530

    Change NODE_ENV for mocha

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
-const config = require('./config');
-const dbconnect = require('./config/dbconnect');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+const config = require("./config");
+const dbconnect = require("./config/dbconnect");
 
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
-if (process.env.NODE_ENV !== 'test') {
-	app.use(morgan('combined'));
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("combined"));
 }
 
 routes(app);
 
 dbconnect();
 
-app.listen(config.port, () => {
-	console.log(`Server started on port ${config.port}`);
+let port = config.port;
+if (process.env.NODE_ENV === "test") port = 8001;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports = app;
