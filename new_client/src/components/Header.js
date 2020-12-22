162f71de3727e2cbeb0c04907e64d102511470c8
Kush Kothari <kushkothari2001@gmail.com>
2020-12-22 16:38:25 +0530

    Made the navbar responsive with a separate mobile view

--- new_client/src/components/Header.js
+++ new_client/src/components/Header.js
@@ -4,39 +4,55 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 export default function Header(props) {
 	const [isDesktop, setisDesktop] = useState(window.innerWidth > 900);
-
+	const [navOpen, setnavOpen] = useState(false);
 	useEffect(() => {
-		console.log("Listener added");
+		function navToggle() {
+			setnavOpen((navPrevState) => {
+				return !navPrevState;
+			});
+		}
 		window.addEventListener("resize", () => {
 			setisDesktop(window.innerWidth > 900);
 		});
+		document.getElementById("nav-toggler").addEventListener("click", navToggle);
 	}, []);
 
 	return (
-		<div className={isDesktop ? "navbar-custom-desk" : ""}>
-			<div className={isDesktop ? "nav-item-list-desk" : ""}>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+		<div className={isDesktop ? "navbar-custom-desk" : "navbar-custom-mob"}>
+			<div
+				className={isDesktop ? "nav-burg-desk" : "nav-burg-mob"}
+				id="nav-toggler"
+			>
+				<i className="fa fa-bars"></i>
+			</div>
+			<div
+				className={
+					(isDesktop ? "nav-item-list-desk" : "nav-item-list-mob") +
+					(navOpen ? " nav-mob-open" : " nav-mob-close")
+				}
+			>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Home
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					About Us
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Events
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Glimpses
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Blogs
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Resources
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					Projects
 				</Link>
-				<Link to="/" className={isDesktop ? "nav-item-desk" : ""}>
+				<Link to="/" className={isDesktop ? "nav-item-desk" : "nav-item-mob"}>
 					{props.loggedIn ? "Logout" : "Login"}
 				</Link>
 			</div>
