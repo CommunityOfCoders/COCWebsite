17515079ba7601bafa3933447bb03fcd767a80f3
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-12-27 19:38:01 +0530

    wip

--- new_client/src/Home/components/Banner/Banner.jsx
+++ new_client/src/Home/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 // @flow
-import React from 'react';
-import './Banner.scss'
-import photo from '../assets/banner.jpeg'
+import React from "react";
+import "./Banner.scss";
+import photo from "../assets/banner.jpeg";
 
 const Banner = () => {
   return (
@@ -9,7 +9,10 @@ const Banner = () => {
       <div className="banner-photo">
         <img className="photo" src={photo} alt="photo" />
         <div className="banner-center">Community of Coders</div>
-        <div className="banner-text"> IMAGINE &nbsp; &nbsp; &nbsp; BELIEVE &nbsp; &nbsp; &nbsp; ACHIEVE</div>
+        <div className="banner-text">
+          {" "}
+          IMAGINE &nbsp; &nbsp; BELIEVE &nbsp; &nbsp; ACHIEVE
+        </div>
       </div>
     </section>
   );
