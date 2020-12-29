276ed9ecdac5b10154b18e0926572fd62274094d
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-12-29 17:34:05 +0530

    Shape of data

--- server/src/controllers/AlumniController.js
+++ server/src/controllers/AlumniController.js
@@ -1,6 +1,16 @@
 module.exports = {
   getAlumniDetails(req, res) {
-    // No idea what we are going to get
+    // Shape of Data
+    /*
+    {
+      Timestamp: [ '12/29/2020 17:07:24' ],
+      'Full Name': [ 'SKG2' ],
+      'Profile URL (social media, portfolio, etc)': [ 'google.com' ],
+      'Current Work Location': [ 'Matunga' ],
+      'Email Address': [ 'shubhankar.gupto.11@gmail.com' ],
+      'Graduation Year': [ '2022' ]
+    }
+    */
     console.log(req.body);
     res.status(200).json({ Status: "OK" });
   },
