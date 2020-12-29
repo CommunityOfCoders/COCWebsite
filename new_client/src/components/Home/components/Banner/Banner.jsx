12909844b8ff44dba61424149b40b2c24cf79df0
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-12-29 10:57:41 +0530

    fix homepage

--- new_client/src/components/Home/components/Banner/Banner.jsx
+++ new_client/src/components/Home/components/Banner/Banner.jsx
@@ -1,4 +1,3 @@
-// @flow
 import React from "react";
 import "./Banner.scss";
 import COC from "../assets/COC.png";
@@ -6,16 +5,13 @@ import COC from "../assets/COC.png";
 const Banner = () => {
   return (
     <section className="Banner">
-      <div className="banner-photo">
-        <div className="parallax"></div>
-        {/* <img className="photo" src={photo} alt="photo" /> */}
-        <div className="banner-center">
-          <img src={COC} style={{ width: 60, height: 60 }} alt="COC Logo PNG" />{" "}
-          Community of Coders
-        </div>
-        <div className="banner-text">
-          &nbsp; IMAGINE &nbsp; | &nbsp; BELIEVE &nbsp; | ACHIEVE &nbsp;
-        </div>
+      <div className="parallax"></div>
+      <div className="banner-center">
+        <img src={COC} style={{ width: 60, height: 60 }} alt="COC Logo PNG" />{" "}
+        Community of Coders
+      </div>
+      <div className="banner-text">
+        &nbsp; IMAGINE &nbsp; | &nbsp; BELIEVE &nbsp; | &nbsp; ACHIEVE &nbsp;
       </div>
     </section>
   );
