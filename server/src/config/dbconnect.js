5cb2c573eed88a430919421f1ebe945fd05e8a47
Saif807380 <saif1204kazi@gmail.com>
2021-01-29 17:55:16 +0530

    set auto indexing to false for production

--- server/src/config/dbconnect.js
+++ server/src/config/dbconnect.js
@@ -7,7 +7,7 @@ function connect() {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    autoIndex: false
+    autoIndex: process.env.NODE_ENV !== "production"
   };
 
   let connectionString = "mongodb://localhost:27017/test";
