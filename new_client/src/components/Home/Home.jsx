391b510fce8778419c1274a2a507eed2e5aa04b1
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2021-02-03 19:36:28 +0530

    setup achievements carousel on homepage

--- new_client/src/components/Home/Home.jsx
+++ new_client/src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "./components/Banner/Banner";
 import Welcome from "./components/Welcome/Welcome";
 import Description from "./components/Description/Description";
 import Highlights from "./components/Highlights/Highlights";
+import Achievements from "./components/Achievements/Achievements";
 
 const Home = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -26,6 +27,7 @@ const Home = () => {
         <Welcome width={width} />
         <Description width={width} />
         <Highlights />
+        <Achievements />
       </article>
     </div>
   );
--- server/src/controllers/AchievementsController.js
+++ server/src/controllers/AchievementsController.js
@@ -26,6 +26,7 @@ module.exports = {
       return res.status(500).json({ error: e.message });
     }
   },
+
   async allAchievements(_,res){
     try{
       const achievements = await Achievement.find({});
@@ -33,5 +34,38 @@ module.exports = {
     } catch(e){
       return res.status(500).json({ error: e.message });
     }
-  }
+  },
+
+  async getDummyAchievements(_, res) {
+    const achievements = [
+      {
+        title: 'First in SIH Hackathon',
+        owner: {
+          fullName: 'Smartest guy',
+          email: 'guy@gmail.com'
+        },
+        imageUrl: 'https://dealersupport.co.uk/wp-content/uploads/2020/03/iStock-659111108-587x381.jpg',
+        projectUrl: 'https://github.com/CommunityOfCoders/COCWebsite'
+      },
+      {
+        title: 'First in ACM-ICPC',
+        owner: {
+          fullName: 'Second smartest guy',
+          email: 'guy2@gmail.com'
+        },
+        imageUrl: 'https://image.freepik.com/free-vector/winner_23-2147506357.jpg?2',
+        projectUrl: 'https://github.com/CommunityOfCoders/COCWebsite'
+      },
+      {
+        title: 'Google internship',
+        owner: {
+          fullName: 'Richest guy',
+          email: 'richguy@gmail.com'
+        },
+        imageUrl: 'https://image.freepik.com/free-vector/winner_23-2147506357.jpg?2',
+        projectUrl: 'https://github.com/CommunityOfCoders/COCWebsite'
+      }
+    ]
+    return res.status(200).json({ achievements })
+  } 
 }
