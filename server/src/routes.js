00bc80e805ff7427b67622c0369e80a877717df0
Shubhankar Kanchan Gupta <shubhankar.gupto.11@gmail.com>
2021-02-03 12:35:40 +0530

    Fixes for deploying to prod (#92)
    * Lazy load all necessary chunks
    * Fixes for deploying to prod
    
    1. Added utility script for adding projects and domains next time.
    2. Removed all cache middlewares.
    3. Remove glimpse from header, rest of the stuff is intact.
    
    * fix broken app
    * fix unnecessary creating hogher dim arr

--- server/src/routes.js
+++ server/src/routes.js
@@ -26,14 +26,14 @@ module.exports = (app) => {
     app.post('/api/new-password', AuthController.newPassword);
 
     //Events Paths
-    app.get('/api/events', cache.getFromCache, Events.getEvents, cache.setCache); // Tested
-    app.post('/api/events', auth.loginRequired, user.isMember, upload.single('COC_Event'), Events.uploadEvent, cache.deleteCache)
-    app.put('/api/events/:id', auth.loginRequired, user.isMember, upload.single('COC_Event'),Events.updateEvent, cache.deleteCache);
+    app.get('/api/events', Events.getEvents); // Tested
+    app.post('/api/events', auth.loginRequired, user.isMember, upload.single('COC_Event'), Events.uploadEvent)
+    app.put('/api/events/:id', auth.loginRequired, user.isMember, upload.single('COC_Event'),Events.updateEvent);
     app.put('/api/events/form', auth.loginRequired, user.isMember, Events.addForm) // Tested
     app.get('/api/events/:id', Events.getEventById); // Tested
     app.post('/api/events/reminder', Events.addReminder);
     app.delete('/api/events/reminder/:id', Events.cancelReminder);
-    app.delete('/api/events/:id', auth.loginRequired, user.isMember, Events.deleteEvent, cache.deleteCache); // Tested
+    app.delete('/api/events/:id', auth.loginRequired, user.isMember, Events.deleteEvent); // Tested
 
     // Registration
     app.post('/api/reg-form', Register.regForm); // Incomplete controller
@@ -55,35 +55,35 @@ module.exports = (app) => {
     app.delete('/api/blogs/delete/:id', auth.loginRequired, blog.isBlogWritten, Blogs.deleteBlogById); // Tested
 
     // Project Domains
-    app.get('/api/domains', cache.getFromCache, DomainController.allDomains, cache.setCache); // Tested
+    app.get('/api/domains', DomainController.allDomains); // Tested
     app.get('/api/domains/:id', DomainController.viewDomainById); // Tested
-    app.post('/api/domains/new', auth.loginRequired, user.isMember, DomainController.createDomain, cache.deleteCache); // Tested
-    app.put('/api/domains/edit/:id', auth.loginRequired, user.isMember, DomainController.editDomainById, cache.deleteCache); // Tested
-    app.delete('/api/domains/delete/:id', auth.loginRequired, user.isMember, DomainController.deleteDomainById, cache.deleteCache); // Tested
+    app.post('/api/domains/new', auth.loginRequired, user.isMember, DomainController.createDomain); // Tested
+    app.put('/api/domains/edit/:id', auth.loginRequired, user.isMember, DomainController.editDomainById); // Tested
+    app.delete('/api/domains/delete/:id', auth.loginRequired, user.isMember, DomainController.deleteDomainById); // Tested
 
     // Projects
-    app.get('/api/projects', cache.getFromCache, ProjectController.allProjects, cache.setCache); // Tested
+    app.get('/api/projects', ProjectController.allProjects); // Tested
     app.get('/api/projects/filter/:id', ProjectController.viewProjectsByDomain); // Tested
     app.get('/api/projects/:id', ProjectController.viewProjectById); // Tested
-    app.post('/api/projects/new', auth.loginRequired, user.isMember, ProjectController.createProject, cache.deleteCache); // Tested
-    app.delete('/api/projects/delete/:id', auth.loginRequired, user.isMember, ProjectController.deleteProjectById, cache.deleteCache); // Tested
+    app.post('/api/projects/new', auth.loginRequired, user.isMember, ProjectController.createProject); // Tested
+    app.delete('/api/projects/delete/:id', auth.loginRequired, user.isMember, ProjectController.deleteProjectById); // Tested
 
     // Alumni
-    app.get('/api/alumni', cache.getFromCache, AlumniController.allAlumni, cache.setCache); // Tested
-    app.post('/api/alumni', AlumniController.createAlumnus, cache.deleteCache); // Tested
+    app.get('/api/alumni', AlumniController.allAlumni); // Tested
+    app.post('/api/alumni', AlumniController.createAlumnus); // Tested
 
     // Resources
-    app.get('/api/topics', cache.getFromCache, ResourcesController.getAllTopics, cache.setCache);
+    app.get('/api/topics', ResourcesController.getAllTopics);
     app.get('/api/resources/:id', ResourcesController.getResourceById);
     app.get('/api/topics/:id', ResourcesController.getTopicById);
     app.post('/api/resources/add', auth.loginRequired, user.isMember, ResourcesController.addResource);
-    app.post('/api/topics/add', auth.loginRequired, user.isMember, ResourcesController.addTopic, cache.deleteCache);
+    app.post('/api/topics/add', auth.loginRequired, user.isMember, ResourcesController.addTopic);
     app.put('/api/resources/edit/:id', auth.loginRequired, user.isMember, ResourcesController.updateResourceById);
-    app.put('/api/topics/edit/:id', auth.loginRequired, user.isMember, ResourcesController.updateTopicById, cache.deleteCache);
+    app.put('/api/topics/edit/:id', auth.loginRequired, user.isMember, ResourcesController.updateTopicById);
     app.delete('/api/resources/delete/:id', auth.loginRequired, user.isMember, ResourcesController.deleteResourceById);
-    app.delete('/api/topics/delete/:id', auth.loginRequired, user.isMember, ResourcesController.deleteTopicById, cache.deleteCache);
+    app.delete('/api/topics/delete/:id', auth.loginRequired, user.isMember, ResourcesController.deleteTopicById);
 
     // Achievements
-    app.get('/api/achievements', cache.getFromCache, AchievementsController.allAchievements, cache.setCache);
-    app.post('/api/achievements', AchievementsController.createAchievement, cache.deleteCache);
+    app.get('/api/achievements', AchievementsController.allAchievements);
+    app.post('/api/achievements', AchievementsController.createAchievement);
 }
