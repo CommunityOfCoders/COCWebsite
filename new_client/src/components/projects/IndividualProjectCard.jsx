13f2021650918eb78796ca6679b222ccc56f2f6c
BhavyaSheth22 <60400357+BhavyaSheth22@users.noreply.github.com>
2021-02-17 14:57:15 +0530

    Add modal to project cards (#117)
    * Add modal to project cards
    
    * Modified modal UI and behaviour
    
    1. Added responsive breakpoints to change modal width
    2. Added image to modal
    3. Set scrollable-y behaviour for overflow
    
    * changed modal anchor tag to button
    
    * changed img width

--- new_client/src/components/projects/IndividualProjectCard.jsx
+++ new_client/src/components/projects/IndividualProjectCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -6,8 +6,10 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { overflowEllipsis } from "../Utilities/overflowEllipsis";
+import ProjectModal from "./ProjectModal";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 500,
     margin: "auto",
@@ -31,37 +33,74 @@ export default function IndividualProjectCard({
   shortDesc,
   linkToRepo,
 }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState({});
+
   const classes = useStyles();
+  const displayDesc = overflowEllipsis(shortDesc);
   let domainNames = [];
+
   domains.forEach((domain) => {
     domainNames.push(domain["domainName"]);
   });
 
+  const handleShow = (data) => {
+    setIsModalOpen(true);
+    setModalInfo(data);
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+    setModalInfo({});
+  };
+
   return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2" align="center">
-          {title}
-        </Typography>
-        <Typography variant="subtitle" component="h5" align="center">
-          {domainNames.join(",")}
-        </Typography>
-        {imgSrc.length ? (
-          <React.Fragment>
-            <br />
-            <CardMedia className={classes.media} image={imgSrc} title={title} />
-          </React.Fragment>
-        ) : null}
-        <br />
-        <Typography variant="body" component="p" align="center">
-          {shortDesc}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <a href={linkToRepo} target="_blank" rel="noopener noreferrer">
-          <Button size="small">View project</Button>
-        </a>
-      </CardActions>
-    </Card>
+    <div>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h2" align="center">
+            {title}
+          </Typography>
+          <Typography variant="subtitle1" component="h5" align="center">
+            {domainNames.join(",")}
+          </Typography>
+          {imgSrc.length ? (
+            <React.Fragment>
+              <br />
+              <CardMedia
+                className={classes.media}
+                image={imgSrc}
+                title={title}
+              />
+            </React.Fragment>
+          ) : null}
+          <br />
+          <Typography variant="body1" component="p" align="center">
+            {displayDesc.display}
+            {displayDesc.isOverflow ? (
+              <Button
+                color="primary"
+                onClick={() => {
+                  handleShow({ title: title, desc: shortDesc, image: imgSrc });
+                }}
+              >
+                (read more)
+              </Button>
+            ) : null}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <a href={linkToRepo} target="_blank" rel="noopener noreferrer">
+            <Button size="small">View project</Button>
+          </a>
+        </CardActions>
+      </Card>
+
+      <ProjectModal
+        open={isModalOpen}
+        modalHide={handleClose}
+        projectData={modalInfo}
+      />
+    </div>
   );
 }
