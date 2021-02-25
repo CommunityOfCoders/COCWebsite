3c47b5e6f5cb55fdc50d47e5b4ee20dd03bda244
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-02-25 16:20:55 +0530

    not logged in -> login to rsvp

---
+++ new_client/src/components/events/RegisterButton.jsx
@@ -0,0 +1,40 @@
+import { Button, Tooltip } from "@material-ui/core";
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+export const RegisterButton = ({
+  isAuthenticated,
+  isUserRegistered,
+  handleRSVP,
+  eventID,
+}) => {
+  if (!isAuthenticated) {
+    return (
+      <Tooltip title="You need to login first!">
+        <Link to="/signin">
+          <Button variant="contained" color="primary">
+            {!isUserRegistered ? "Register" : "Unregister"}
+          </Button>
+        </Link>
+      </Tooltip>
+    );
+  }
+  return (
+    <Button
+      variant="contained"
+      color={!isUserRegistered ? "primary" : "secondary"}
+      onClick={() => {
+        handleRSVP(eventID, isUserRegistered);
+      }}
+    >
+      {!isUserRegistered ? "Register" : "Unregister"}
+    </Button>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(RegisterButton);
