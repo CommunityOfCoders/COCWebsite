750ae29c1070a63fa9efd76386096fa39f68ee77
Atharva Thorve <aaathorve@gmail.com>
2021-02-28 21:18:03 +0530

    Alert backdrop (#132)
    * Backdrop utility added
    
    * register if isFuture, event card on mobile
    
    Co-authored-by: ShubhankarKG <shubhankar.gupto.11@gmail.com>

--- new_client/src/components/Utilities/Alert.jsx
+++ new_client/src/components/Utilities/Alert.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { makeStyles, Snackbar } from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
+import Backdrop from "./Backdrop";
 
 const useStyles = makeStyles((theme) => ({
   alert: {
     width: "100%",
+    zIndex: "500",
     "& > * + *": {
       marginTop: theme.spacing(2),
     },
@@ -18,22 +20,25 @@ const useStyles = makeStyles((theme) => ({
 const Alert = (props) => {
   const classes = useStyles();
   return (
-    <div className={classes.alert}>
-      <Snackbar
-        open={props.open}
-        autoHideDuration={props.duration}
-        onClose={props.onCloseHandler}
-      >
-        <MuiAlert
-          elevation={6}
-          variant="filled"
+    <React.Fragment>
+      <Backdrop show={props.open} />
+      <div className={classes.alert}>
+        <Snackbar
+          open={props.open}
+          autoHideDuration={props.duration}
           onClose={props.onCloseHandler}
-          severity={props.severity}
         >
-          {props.message}
-        </MuiAlert>
-      </Snackbar>
-    </div>
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            onClose={props.onCloseHandler}
+            severity={props.severity}
+          >
+            {props.message}
+          </MuiAlert>
+        </Snackbar>
+      </div>
+    </React.Fragment>
   );
 };
 
