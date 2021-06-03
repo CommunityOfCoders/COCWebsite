daf3fade84a198e5ab9fbaaeb6fefe4ccaf56174
Rakesh Rajpurohit <rakesh.rajpurohit.rr.rr.201@gmail.com>
2021-06-03 17:44:47 +0000

    member names converted to  format

--- new_client/src/components/about-us/details.js
+++ new_client/src/components/about-us/details.js
@@ -1,46 +1,46 @@
 export default [
   {
-    name: "Vedant",
+    name: "Vedant Paranjape",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393556/Vedant_Paranjape_eiqieg.jpg",
   },
   {
-    name: "Yash",
+    name: "Yash Tailor",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393524/Yash_Tailor_hpenvu.jpg",
   },
   {
-    name: "Sneha",
+    name: "Sneha Balwani",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393527/Sneha_Balwani_xhmbyo.jpg",
   },
   {
-    name: "Janvi",
+    name: "Janvi Raina",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393525/Janvi_Raina_aujlcf.jpg",
   },
   {
-    name: "Yogesh",
+    name: "Yogesh Deshpande",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393523/Yogesh_Deshpande_onti7z.jpg",
   },
   {
-    name: "Yukta",
+    name: "Yukta Sarode",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393524/YuktaSarode_gooyou.jpg",
   },
   {
-    name: "Bhavya",
+    name: "Bhavya Sheth",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393524/Bhavya_Sheth_iysygc.jpg",
   },
   {
-    name: "Siddhesh_Bandgar",
+    name: "Siddhesh Bandgar",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393523/Siddhesh_Bandgar_v3anih.jpg",
   },
   {
-    name: "Aditya_Patkar",
+    name: "Aditya Patkar",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393522/Aditya_Patkar_dd8hyh.png",
   },
@@ -50,107 +50,107 @@ export default [
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393522/Sarakshi_ipyknv.jpg",
   },
   {
-    name: "Siddharth_Nayak",
+    name: "Siddharth Nayak",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393521/Siddharth_Nayak_q7y6vk.png",
   },
   {
-    name: "Saurav_Kanegaonkar",
+    name: "Saurav Kanegaonkar",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393520/Saurav_Kanegaonkar_gvzreu.jpg",
   },
   {
-    name: "Sayali_Kulkarni",
+    name: "Sayali Kulkarni",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393520/Sayali_Kulkarni_h8lkf6.jpg",
   },
   {
-    name: "Sarang_Shekokar",
+    name: "Sarang Shekokar",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393519/Sarang_Shekokar_xamvnn.jpg",
   },
   {
-    name: "Rohan_vora",
+    name: "Rohan vora",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393519/Rohan_vora_ec1sb9.jpg",
   },
   {
-    name: "Parijat_Dhalkar",
+    name: "Parijat Dhalkar",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393518/Parijat_Dhalkar_tc5rt1.jpg",
   },
   {
-    name: "Riya_Gupta",
+    name: "Riya Gupta",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393518/Riya_Gupta_wozxpc.jpg",
   },
   {
-    name: "Rasika_Chavan",
+    name: "Rasika Chavan",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393517/Rasika_Chavan_tl3hj2.jpg",
   },
   {
-    name: "Sagar_Dholakia",
+    name: "Sagar Dholakia",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393517/Sagar_Dholakia_ieic7q.png",
   },
   {
-    name: "Mohammed_Mehdi",
+    name: "Mohammed Mehdi",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393515/Mohammed_Mehdi_r4sjg9.jpg",
   },
   {
-    name: "Priyesh_Vakharia",
+    name: "Priyesh Vakharia",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393510/Priyesh_Vakharia_nvmfes.jpg",
   },
   {
-    name: "Akshat_Shah",
+    name: "Akshat Shah",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393510/Akshat_Shah_ylmb02.jpg",
   },
   {
-    name: "Hrim_Gandhi",
+    name: "Hrim Gandhi",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393506/Hrim_Gandhi_tbqj0t.jpg",
   },
   {
-    name: "Priyanshi_Gupta",
+    name: "Priyanshi Gupta",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393505/Priyanshi_Gupta_tjuy9b.jpg",
   },
   {
-    name: "Parth_Shah",
+    name: "Parth Shah",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393504/Parth_Shah_fqgxmb.jpg",
   },
   {
-    name: "Palak_Mantry",
+    name: "Palak Mantry",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393502/Palak_Mantry_i1wcxt.jpg",
   },
   {
-    name: "Abhay_Kaul",
+    name: "Abhay Kaul",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393499/Abhay_Kaul_st32uv.jpg",
   },
   {
-    name: "Gaurav_Sharma",
+    name: "Gaurav Sharma",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393499/Gaurav_Sharma_oo56kg.jpg",
   },
   {
-    name: "DevanshiShah",
+    name: "Devanshi Shah",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393499/DevanshiShah_kwo6aq.jpg",
   },
   {
-    name: "Atharva_Thorve",
+    name: "Atharva Thorve",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393498/Atharva_Thorve_uzssqr.jpg",
   },
   {
-    name: "archeel",
+    name: "archeel Parikh",
     img:
       "https://res.cloudinary.com/dxleddac7/image/upload/v1622393498/archeel_xc3w5k.jpg",
   },
